aa4bf00148d13e8b6ee687c7a4b217f57c78e282
Maxwell Yang <gabrielyang233@outlook.com>
2022-01-27 21:32:34 -0500

    Add database initialization and configuration in  and define models in

---
+++ api/database.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+# from sqlalchemy.orm.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+# We can put the uri in plain text here. In the future we need to make it read
+# from the environment
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:helloworld@localhost/debateit"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
---
+++ api/models.py
@@ -0,0 +1,46 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Text, DateTime, LargeBinary
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+from .database import Base
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True)
+    password = Column(String, nullable=False)
+
+class Topic(Base):
+    __tablename__ = "topic"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True)
+    description = Column(DateTime, default="")
+    num_of_debates = Column(Integer, default=0)
+
+    creator_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+
+class Debate(Base):
+    __tablename__ = "debate"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nth_time_of_debate = Column(Integer, nullable=False)
+    start_time = Column(DateTime(timezone=True), server_default=func.now())
+
+    topic_id = Column(Integer, ForeignKey("topic.id"), nullable=False)
+    pro_user_id = Column(Integer, ForeignKey("user.id"))
+    con_user_id = Column(Integer, ForeignKey("user.id"))
+    first_recording_id = Column(Integer, ForeignKey("recording.id"))
+    last_recording_id = Column(Integer, ForeignKey("recording.id"))
+
+class Recording(Base):
+    __tablename__ = "recording"
+
+    id = Column(Integer, primary_key=True, index=True)
+    audio_content = Column(LargeBinary)
+
+    debate_id = Column(Integer, ForeignKey("debate.id"), nullable=False)
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    prev_recording_id = Column(Integer, ForeignKey("recording.id"))
+    next_recording_id = Column(Integer, ForeignKey("recording.id"))
