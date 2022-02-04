ad0019b672e90abc5172566c0a05dbf9d4dc4814
Maxwell Yang <gabrielyang233@outlook.com>
2022-02-03 22:18:29 -0500

    ADD Navigation Bar(WIP)

---
+++ web/src/Theme.js
@@ -0,0 +1,17 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#36558f',
+        },
+        secondary: {
+            main: '#40376e',
+        },
+        info: {
+            main: '#ffffff',
+        },
+    }
+})
+
+export default theme;
---
+++ web/src/components/navbar.js
@@ -0,0 +1,91 @@
+import '@fontsource/bebas-neue';
+import '@fontsource/nunito';
+
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Drawer from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import { ThemeProvider } from '@mui/material/styles';
+
+import theme from '../Theme';
+
+function NavBar(props) {
+	return (
+		<ThemeProvider theme={theme}>
+			<Drawer
+				variant="permanent"
+				anchor="left"
+				open={true}
+			>
+				<Box
+					component="nav"
+					sx={{
+						width: 250,
+						bgcolor: 'secondary.main'
+					}}
+				>
+					<List>
+						<ListItem key="-2">
+							<ListItemText
+								primary={
+									<Typography
+										variant="h4"
+										component="div"
+										align="center"
+										sx={{
+											fontFamily: "Bebas Neue",
+											color: "info.main"
+										}}
+									>
+										DebateIt
+									</Typography>
+								}
+							/>
+						</ListItem>
+						<ListItem key="-1">
+							<ListItemText
+								primary={
+									<Typography
+										variant="h6"
+										component="div"
+										align="center"
+										mt={4}
+										mb={4}
+										sx={{
+											fontFamily: "Bebas Neue",
+											color: "info.main"
+										}}
+									>
+										ALPACAMAX
+									</Typography>
+								}
+							/>
+						</ListItem>
+						{["Yours", "Topics", "Debates"].map((text, index) => (
+							<ListItem button key={index}>
+								<ListItemText
+									primary={
+										<Typography
+											component="div"
+											align="center"
+											sx={{
+												fontFamily: "Nunito",
+												color: "info.main"
+											}}
+										>
+											{text}
+										</Typography>
+									}
+								/>
+							</ListItem>
+						))}
+					</List>
+				</Box>
+			</Drawer>
+		</ThemeProvider>
+	);
+};
+
+export default NavBar;
