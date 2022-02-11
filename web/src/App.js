c481737f9ec7ba11aeffa960612c8d06886209f1
Maxwell Yang <31149339+AlpacaMax@users.noreply.github.com>
2022-02-10 21:26:54 -0500

    ADD User Login Page (#20)

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,12 @@
 import NavBar from './components/navbar';
 import User from './components/User';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App columns m-0">
         <NavBar />
-        <User />
+        <Login />
     </div>
   );
 };
