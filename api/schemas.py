0de35e57c20cec454ebd0c7ea6f9fd6b0a632cdb
flyhawk86 <2567115879@qq.com>
2022-02-21 17:55:23 -0500

    CHG Remove unnecessary comments

--- api/schemas.py
+++ api/schemas.py
@@ -26,12 +26,6 @@ class CreateTopic(Topic):
         db.close()
         return v
 
-    # @validator("num_of_debates")
-    # def is_num_of_debates_leq_than_0(cls, v):
-    #     if v < 0:
-    #         raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail="Invalid Input of Number of Debates")
-    #     return v
-
     @validator("name")
     def check_topic_name_existed(cls, v):
         if v is None:
@@ -53,12 +47,6 @@ class UpdateTopic(Topic):
         db.close()
         return v
 
-    # @validator("num_of_debates")
-    # def is_num_of_debates_leq_than_0(cls, v):
-    #     if v < 0:
-    #         raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail="Invalid input of number of debates")
-    #     return v
-
     @validator("id")
     def is_topic_existed_by_id(cls, v):
         db = SessionLocal()
@@ -66,9 +54,3 @@ class UpdateTopic(Topic):
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Topic Not Found")
         db.close()
         return v
-
-    # @validator("name")
-    # def check_topic_name_existed(cls, v):
-    #     if v is None:
-    #         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Please Provide a Topic Name")
-    #     return v
