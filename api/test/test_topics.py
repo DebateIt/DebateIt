77a8ee06c03d682197063231a92a4640f9533509
flyhawk86 <2567115879@qq.com>
2022-03-19 17:28:14 -0400

    ADD Testing scripts for Topic

---
+++ api/test/test_topics.py
@@ -0,0 +1,216 @@
+from fastapi.testclient import TestClient
+from ..main import app
+from ..database import SessionLocal
+import pytest
+from ..auth import verify_password
+
+
+client = TestClient(app)
+global token
+global creator_id
+global topic_id
+
+
+# Topic related tests
+@pytest.fixture()
+def init_db():
+    client.get("/utils/seed")
+
+    # Create an user for later topic testing
+    res = client.post("/user", json={"username": "TestUser", "password": "666"})
+    assert verify_password("666", res.json().get("password")) is True
+    assert res.status_code == 201
+    global creator_id
+    creator_id = int(res.json().get("id"))
+    assert creator_id >= 0
+
+    # Retrieve token for later CRUD operations
+    res = client.post("/login", json={"username": "TestUser", "password": "666"})
+    global token
+    token = res.json().get("token_content")
+    assert res.status_code == 200
+
+
+def test_create_topic(init_db):
+    # Create a topic
+    res = client.post(
+        "/topic",
+        json={"name": "TestTopic1", "description": "a dummy testing topic",
+              "creator_id": creator_id, "num_of_debates": 168},
+        headers={"Authorization": "Bearer " + token},
+    )
+    assert res.status_code == 201 # check status code
+
+    global topic_id
+    topic_id = int(res.json().get("id"))
+    assert topic_id > 0
+
+    # pass an existed name (use a topic created in the crud seed api)
+    res = client.post(
+        "/topic",
+        json={"name": "Should we ban racial slur on social media?", "description": "a dummy testing topic",
+              "creator_id": creator_id, "num_of_debates": 168},
+        headers={"Authorization": "Bearer " + token},
+    )
+    assert res.status_code == 400  # check status code
+
+    # pass a JSON body without name
+    res = client.post(
+        "/topic",
+        json={"description": "a dummy testing topic",
+              "creator_id": creator_id, "num_of_debates": 168},
+        headers={"Authorization": "Bearer " + token},
+    )
+    assert res.status_code == 422  # check status code
+
+    # pass a JSON body without creator_id
+    res = client.post(
+        "/topic",
+        json={"name": "TestTopic2", "description": "a dummy testing topic",
+              "num_of_debates": 168},
+        headers={"Authorization": "Bearer " + token},
+    )
+    assert res.status_code == 201  # check status code
+
+    # pass a JSON body with a creator id that is not existed
+    res = client.post(
+        "/topic",
+        json={"name": "TestTopic3", "description": "a dummy testing topic",
+              "creator_id": -1, "num_of_debates": 168},
+        headers={"Authorization": "Bearer " + token},
+    )
+    assert res.status_code == 201  # check status code
+
+    # pass num_of_debates with a negative number
+    res = client.post(
+        "/topic",
+        json={"name": "TestTopic4", "description": "a dummy testing topic",
+              "creator_id": creator_id, "num_of_debates": -1},
+        headers={"Authorization": "Bearer " + token},
+    )
+    assert res.status_code == 422  # check status code
+
+
+def test_read_topic():
+    # Read the newly created topic
+    res = client.get(
+        "/topic/"+str(topic_id),
+        headers={"Authorization": "Bearer " + token},
+    )
+    assert res.status_code == 200  # check status code
+
+    # Read a topic that doesn't exist
+    res = client.get(
+        "/topic/0",
+        headers={"Authorization": "Bearer " + token},
+    )
+    assert res.status_code == 404  # check status code
+
+    # Pass a URL param without topic id
+    res = client.get(
+        "/topic/",
+        headers={"Authorization": "Bearer " + token},
+    )
+    assert res.status_code == 405  # check status code
+
+
+def test_update_topic():
+    # Update the newly created topic
+    res = client.put(
+        "/topic/"+str(topic_id),
+        json={"name": "TestTopic1", "description": "topic is updated",
+              "creator_id": creator_id, "num_of_debates": 16888888},
+        headers={"Authorization": "Bearer " + token},
+    )
+    assert res.status_code == 200  # check status code
+
+    # Pass a JSON body without name
+    res = client.put(
+        "/topic/" + str(topic_id),
+        json={"description": "topic is updated",
+              "creator_id": creator_id, "num_of_debates": 16888888},
+        headers={"Authorization": "Bearer " + token},
+    )
+    assert res.status_code == 200  # check status code
+
+    # Pass a JSON body without description
+    res = client.put(
+        "/topic/" + str(topic_id),
+        json={"name": "TestTopic1",
+              "creator_id": creator_id, "num_of_debates": 16888888},
+        headers={"Authorization": "Bearer " + token},
+    )
+    assert res.status_code == 200  # check status code
+
+    # Pass a JSON body without creator id
+    res = client.put(
+        "/topic/" + str(topic_id),
+        json={"name": "TestTopic1", "description": "topic is updated",
+              "num_of_debates": 16888888},
+        headers={"Authorization": "Bearer " + token},
+    )
+    assert res.status_code == 200  # check status code
+
+    # Pass a JSON body without the number of debates
+    res = client.put(
+        "/topic/" + str(topic_id),
+        json={"name": "TestTopic1", "description": "topic is updated",
+              "creator_id": creator_id},
+        headers={"Authorization": "Bearer " + token},
+    )
+    assert res.status_code == 200  # check status code
+
+    # Update a topic that doesn't exist
+    res = client.put(
+        "/topic/0",
+        json={"name": "TestTopic1", "description": "topic is updated",
+              "creator_id": creator_id, "num_of_debates": 16888888},
+        headers={"Authorization": "Bearer " + token},
+    )
+    assert res.status_code == 404  # check status code
+
+    # Pass a URL param without topic id
+    res = client.put(
+        "/topic/",
+        json={"name": "TestTopic1", "description": "topic is updated",
+              "creator_id": creator_id, "num_of_debates": 16888888},
+        headers={"Authorization": "Bearer " + token},
+    )
+    assert res.status_code == 307  # check status code
+
+    # Pass a JSON body with the old topic name
+    res = client.put(
+        "/topic/" + str(topic_id),
+        json={"name": "TestTopic1", "description": "topic is updated",
+              "creator_id": creator_id, "num_of_debates": 16888888},
+        headers={"Authorization": "Bearer " + token},
+    )
+    assert res.status_code == 200  # check status code
+
+    # Pass a JSON body with an existing topic name
+    res = client.put(
+        "/topic/" + str(topic_id),
+        json={"name": "Should we ban racial slur on social media?", "description": "topic is updated",
+              "creator_id": creator_id, "num_of_debates": 16888888},
+        headers={"Authorization": "Bearer " + token},
+    )
+    assert res.status_code == 400  # check status code
+
+
+def test_delete_topic():
+    # Delete the newly created topic
+    res = client.delete(
+        "/topic/" + str(topic_id),
+        headers={"Authorization": "Bearer " + token},
+    )
+    assert res.status_code == 200  # check status code
+
+    # Delete a topic that is not existed
+    res = client.get(
+        "/topic/" + str(topic_id),
+        headers={"Authorization": "Bearer " + token},
+    )
+    assert res.status_code == 404  # check status code
+
+    # Reseed all tables
+    client.get("/utils/seed")
