4f04595ad2f02cc285423158905d23ddee26b731
Maxwell Yang <gabrielyang233@outlook.com>
2022-03-23 10:42:17 -0400

    FIX Message Box and Send Button

--- web/src/pages/DebateText.jsx
+++ web/src/pages/DebateText.jsx
@@ -1,24 +1,42 @@
 import React from 'react';
 
-import InputBox from '../components/inputbox';
+import ChatInputBox from '../components/chatinputbox';
 
 function DebateText() {
     return (
-        <div className="column is-flex-direction-column">
+        <div className="column is-flex is-flex-direction-column">
             <div className="level">
-                <div className="level-item has-text-left">
-                    <p className="title has-text-white">For</p>
+                <div className="level-left">
+                    <div className="level-item">
+                        <p className="title has-text-white">For</p>
+                    </div>
                 </div>
-                <div className="level-item has-text-right">
-                    <p className="title has-text-white">Against</p>
+                <div className="level-right">
+                    <div className="level-item has-text-right">
+                        <p className="title has-text-white">Against</p>
+                    </div>
                 </div>
             </div>
-            <nav class="navbar is-fixed-bottom">
-                <InputBox
-                    className="is-flex-grow-1"
-                    name="Message"
-                />
-            </nav>
+            <div className="is-flex-grow-1">
+
+            </div>
+            <div className="columns is-variable is-1">
+                <div className="column is-11">
+                    <ChatInputBox
+                        name="Send Your Argument and Rebuttal"
+                    />
+                </div>
+                <div className="column">
+                    <div className="control">
+                        <button
+                            type="button"
+                            className="button is-success has-text-info is-fullwidth"
+                        >
+                            Send
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
