499d5d3c1d781b860a8a393385f808719cd53c32
Maxwell Yang <gabrielyang233@outlook.com>
2022-03-31 23:06:23 -0400

    ADD Setup debate room

---
+++ api/debate/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+PRO_USER_ID = None
+CON_USER_ID = None
+curr_turn   = None
+
+@app.get("/history")
+def get_debate_history():
+    return None
+
+@app.post("/message")
+def send_message():
+    return None
